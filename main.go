5dfc39e4b7cfd01d97dde98d3c5077687be94aea
jeremy <jeremyeparker@yahoo.com>
2022-11-09 12:20:04 -0500

    different ways to format strings to output

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world ")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	// fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	// fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are stil available")
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
 }

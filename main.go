6e647945bf1f8ef999ead1908b24964619f01073
jeremy <jeremyeparker@yahoo.com>
2022-11-10 11:38:19 -0500

    remove tickets based on user input

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	//ask user for their name
 	// fmt.Println(remainingTickets)
 	// fmt.Println(&remainingTickets) //prints the memory address of the variable remainingTickets
@@ -37,5 +37,8 @@ func main() {
 	fmt.Println("- Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets.\n You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
 }
